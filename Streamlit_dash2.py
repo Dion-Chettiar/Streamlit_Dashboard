df12f4f692868fc3c2c495b15764af78dd920e4c
Dion-Chettiar <86397102+Dion-Chettiar@users.noreply.github.com>
2025-06-25 15:43:14 +0100

    Update Streamlit_dash2.py

--- Streamlit_dash2.py
+++ Streamlit_dash2.py
@@ -1,11 +1,5 @@
 import streamlit as st
-import graphviz
 
-try:
-    import graphviz
-    st.success("Graphviz imported successfully!")
-except ModuleNotFoundError as e:
-    st.error(f"Graphviz not found: {e}")
 
 
 st.title("Optimal Combinations")
